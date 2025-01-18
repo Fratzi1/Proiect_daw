27af92c1aafb60fcc16ae1559af564dcc2c9119a
Vladone <vladfratila2000@yahoo.com>
2025-01-18 14:41:32 +0200

    am bagat datele din app

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,13 @@
 <?php
-    $db_pass = trim(file_get_contents("dbpass.txt"));
+
+    $server = 'nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $username = 'snlf1e75emr6xu8z';
+    $password = 'xaysrwe724xapov3';
+    $database = 'ze4xiva4lm9mohzr';
+    $port = 3306;
 
     // Sintaxa: mysqli_connect(host, user,pass, database_name)
-    $link = new mysqli("localhost", "v",$db_pass,'proiect_daw');
+    $link = new mysqli($server, $username, $password, $database, $port);
 
     if(!$link){
         echo ('Error!unable to connect to MySql!');
@@ -10,5 +15,4 @@
     } else {
         // echo('Succesfully connected to Mysql');
     }
-    
 ?>
